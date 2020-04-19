f03dcead42665babce6d4a7e6af7d544a67bbab8
github username <github email address>
2020-04-19 13:15:00 -0400

    Done.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+document.getElementById('modal').setAttribute('class', 'hidden');
 
 // Your JavaScript code goes here!
 
@@ -13,16 +14,15 @@ function addLikeCallback(e){
       console.log(serverMessage)
       heart.innerHTML = FULL_HEART;
       heart.style.color = 'red';
-      errorModal.style.display = 'none';
+      errorModal.setAttribute('class', 'hidden');
     })
     .catch(function(error){
       console.log(error)
-      errorModal.style.display = '';
+      errorModal.removeAttribute('class');
     })
 }
 
 document.addEventListener('DOMContentLoaded', function(){  
-  document.getElementById('modal').style.display = 'none';
   const likeBtns = document.querySelectorAll('span.like-glyph');
   for(const btn of likeBtns){
     btn.addEventListener('click', addLikeCallback);
